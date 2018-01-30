ff7255c7e4c4dd8ad7ca9a3704815d3df7451503
Ron MacNeil <macro@hotmail.com>
2018-01-30 22:31:47 +1000

    Add new C# project TypeId

--- MacroSln/VisualStudioProjectTypeIds.cs
+++ MacroSln/VisualStudioProjectTypeIds.cs
@@ -18,6 +18,9 @@ public static readonly string
 CSharp = "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}";
 
 public static readonly string
+CSharpNew = "{9A19103F-16F7-4668-BE54-9A1E7A4F7556}";
+
+public static readonly string
 VisualBasic = "{F184B08F-C81C-45F6-A57F-5ABD9991F28F}";
 
 
